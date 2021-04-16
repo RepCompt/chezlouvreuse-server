6ea3fdfe5bd8a90a3bf1bdb613830233e125417c
AMINE1921 <amineoubaya1921@gmail.com>
2021-04-16 06:59:09 +0200

    chez louvreuse v1

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Le serveur est lancé sur le port: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Le serveur est lancé sur le port: ${PORT}`);
+// });
+
+app.listen();
